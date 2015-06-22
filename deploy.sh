003e92821a5aceb9a46d7a3b496afad43aec41b2
Jaggy Gauran <jaggygauran@gmail.com>
2015-06-22 16:19:56 +0800

    Add a deployment script.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/env/bash
+
+cp -R dist res
+
+echo "Checking out github pages."
+git checkout gh-pages
+
+echo "Updating assets"
+rm -rf images index.html css js
+cp -R res/* .
+
+git add images index.html js css
+git commit -m 'Deploying github pages.'
+
+git checkout master
